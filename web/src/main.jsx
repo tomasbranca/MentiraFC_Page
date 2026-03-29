f29fd50e410aeb570e0d8a490742e754634be95f
tomasbranca <tomasbrancatisano@gmail.com>
2026-03-29 18:23:24 -0300

    Reubicacion de scrollToTop.js

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./utils/ScrollToTop.jsx";
+import ScrollToTop from "./app/scrollToTop.js";
 import "./index.css";
 import App from "./App.jsx";
 
