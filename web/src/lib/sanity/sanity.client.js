afed33245f5ddca44a459c2ce25af037686c8aaa
tomasbranca <tomasbrancatisano@gmail.com>
2026-04-07 19:04:11 -0300

    Añadiendo variables al .env

--- web/src/lib/sanity/sanity.client.js
+++ web/src/lib/sanity/sanity.client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
-const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || "jwpxrdo2";
-const dataset = import.meta.env.VITE_SANITY_DATASET || "production";
-const apiVersion = import.meta.env.VITE_SANITY_API_VERSION || "2023-01-01";
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID;
+const dataset = import.meta.env.VITE_SANITY_DATASET;
+const apiVersion = import.meta.env.VITE_SANITY_API_VERSION;
 
 export const client = createClient({
   projectId,
