08068f3bd62071c2d049dc33e229136a1ff3f895
tomasbranca <tomasbrancatisano@gmail.com>
2026-04-12 03:33:31 -0300

    fix: optimize #1

--- web/src/presentation/components/Loader/Loader.tsx
+++ web/src/presentation/components/Loader/Loader.tsx
@@ -3,14 +3,10 @@ import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-violet-950">
+    <div className="fixed inset-0 z-9999 flex items-center justify-center bg-violet-950">
       <div className="flex flex-col items-center gap-6">
         {/* Logo */}
-        <img
-          src="/logo.webp"
-          alt="Mentira FC"
-          className="w-20 fade-in"
-        />
+        <img src="/logo.webp" width={100} height={100} alt="Mentira FC" />
 
         {/* Spinner */}
         <div className="h-10 w-10 animate-spin rounded-full border-2 border-slate-600 border-t-white" />
